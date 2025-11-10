1fc803739333c8a06e733dd768e28a59b2da26db
Joseph Pal <josephpal27@gmail.com>
2025-11-10 18:17:10 +0530

    Home banner added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import '../css/Home.css';
 
-import homeBanner from '/images/banners/home-banner.avif';
+// import homeBanner1 from '/images/banners/home-banner-1.jpg';
+import homeBanner2 from '/images/banners/home-banner-2.jpg';
 
 const Home = () => {
   return (
     <>
       {/* Banner */}
       <div className="banner">
-        <img src={homeBanner} alt="Image" loading="lazy" />
+        <img src={homeBanner2} alt="Image" loading="lazy" />
       </div>
     </>
   )
