0299b28f6aaa2ab956c1a0f6bb42d1ad3d0459a4
Joseph Pal <josephpal27@gmail.com>
2025-11-11 17:20:08 +0530

    Who we are head added

--- src/components/who-we-are/WhoWeAre.jsx
+++ src/components/who-we-are/WhoWeAre.jsx
@@ -1,13 +1,13 @@
 import './WhoWeAre.css';
 
 const WhoWeAre = () => {
-  return (
-    <>
-      <section className="who-we-are">
-
-      </section>
-    </>
-  )
+    return (
+        <>
+            <section className="who-we-are">
+                <h1>WHO WE ARE</h1>
+            </section>
+        </>
+    )
 }
 
 export default WhoWeAre
