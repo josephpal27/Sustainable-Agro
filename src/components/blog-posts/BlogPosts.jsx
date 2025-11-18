a637675c1cb3cc7ed6a5d11bfd38f48de6fa9ce5
Joseph Pal <josephpal27@gmail.com>
2025-11-18 18:09:50 +0530

    Blog Posts heading added

--- src/components/blog-posts/BlogPosts.jsx
+++ src/components/blog-posts/BlogPosts.jsx
@@ -4,7 +4,8 @@ const BlogPosts = () => {
   return (
     <>
       <section className="blog-posts">
-
+        <h5>BLOG <span>POSTS</span></h5>
+        <p>Latin words, combined with a handful of model sentence structures, <br /> to generate Lorem Ipsum which looks reasonable.</p>
       </section>
     </>
   )
