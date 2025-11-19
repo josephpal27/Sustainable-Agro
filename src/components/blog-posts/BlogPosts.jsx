ae2ac3e46ac7ebe2d9770f7c4662f87316401ec4
Joseph Pal <josephpal27@gmail.com>
2025-11-19 12:08:04 +0530

    Blog Posts updated

--- src/components/blog-posts/BlogPosts.jsx
+++ src/components/blog-posts/BlogPosts.jsx
@@ -1,4 +1,8 @@
+import { Link } from 'react-router-dom';
 import './BlogPosts.css';
+import { Button } from '@mui/material';
+
+import blogPost1 from '../../assets/images/blogs/1.jpg';
 
 const BlogPosts = () => {
   return (
@@ -6,6 +10,47 @@ const BlogPosts = () => {
       <section className="blog-posts">
         <h5>BLOG <span>POSTS</span></h5>
         <p>Latin words, combined with a handful of model sentence structures, <br /> to generate Lorem Ipsum which looks reasonable.</p>
+        <div className="blog-posts-row">
+          <div className="blog-post-card">
+            <div className="card-img">
+              <img src={blogPost1} alt="Blog Image" loading="lazy" />
+            </div>
+            <div className="card-desc">
+              <div className="desc-content">
+                <span className="blog-sub-title">DIET / TIPS & GUIDES</span>
+                <span className="blog-title">Tips for Ripening your Fruit</span>
+                <p>The generated Lorem Ipsum is therefore always free from repetition injected humour, or non-characteristic words etc.</p>
+                <Link to="/blog"><Button>Read More</Button></Link>
+              </div>
+            </div>
+          </div>
+          <div className="blog-post-card">
+            <div className="card-img">
+              <img src={blogPost1} alt="Blog Image" loading="lazy" />
+            </div>
+            <div className="card-desc">
+              <div className="desc-content">
+                <span className="blog-sub-title">DIET / TIPS & GUIDES</span>
+                <span className="blog-title">Tips for Ripening your Fruit</span>
+                <p>The generated Lorem Ipsum is therefore always free from repetition injected humour, or non-characteristic words etc.</p>
+                <Link to="/blog"><Button>Read More</Button></Link>
+              </div>
+            </div>
+          </div>
+          <div className="blog-post-card">
+            <div className="card-img">
+              <img src={blogPost1} alt="Blog Image" loading="lazy" />
+            </div>
+            <div className="card-desc">
+              <div className="desc-content">
+                <span className="blog-sub-title">DIET / TIPS & GUIDES</span>
+                <span className="blog-title">Tips for Ripening your Fruit</span>
+                <p>The generated Lorem Ipsum is therefore always free from repetition injected humour, or non-characteristic words etc.</p>
+                <Link to="/blog"><Button>Read More</Button></Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
