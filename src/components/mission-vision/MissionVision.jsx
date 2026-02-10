724b84c724a5499fb7263707209d8e0119baa27c
Joseph Pal <josephpal27@gmail.com>
2026-02-10 18:18:36 +0530

    Mission Vision Updated

---
+++ src/components/mission-vision/MissionVision.jsx
@@ -0,0 +1,33 @@
+import './MissionVision.css';
+import missionImage from '../../assets/images/mission.png'
+import visionImage from '../../assets/images/vision.png'
+
+const MissionVision = () => {
+    return (
+        <>
+            <section className="mission-vision">
+                <div className="mv-box" id="mission-box">
+                    <div className="mv-box-left">
+                        <span>OUR <br /> MISSION</span>
+                    </div>
+                    <div className="mv-box-right">
+                        <p>To accelerate the transition to net-zero agriculture by providing scalable technologies and expertise that empower growers to feed the world sustainably.</p>
+                    </div>
+                    <img src={missionImage} alt="Mission" loading="lazy" />
+                </div>
+                <h2>MISSION & VISION</h2>
+                <div className="mv-box" id="vision-box">
+                    <div className="mv-box-left">
+                        <p>A world where agriculture regenerates the planet, and energy production works in harmony with nature.</p>
+                    </div>
+                    <div className="mv-box-right">
+                        <span>OUR <br /> VISION</span>
+                    </div>
+                    <img src={visionImage} alt="Vision" loading="lazy" />
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default MissionVision
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import AboutBanner from '../components/about-banner/AboutBanner'
 import AboutFeatured from '../components/about-featured/AboutFeatured'
+import MissionVision from '../components/mission-vision/MissionVision'
 
 const About = () => {
     return (
         <>
             <AboutBanner />
             <AboutFeatured />
+            <MissionVision />
         </>
     )
 }
