96e2103674445fa044df9762d7c9c776f95ef938
Joseph Pal <josephpal27@gmail.com>
2026-04-09 15:34:36 +0530

    Environmental responsiveness added

--- src/pages/EsgImpact.jsx
+++ src/pages/EsgImpact.jsx
@@ -3,6 +3,7 @@ import EsgBanner from "../components/esg-banner/EsgBanner"
 import Governance from "../components/governance/Governance"
 import Reporting from "../components/reporting/Reporting"
 import SocialImpact from "../components/social-impact/SocialImpact"
+import Footer from './../components/footer/Footer';
 
 const EsgImpact = () => {
     return (
@@ -12,6 +13,7 @@ const EsgImpact = () => {
             <SocialImpact />
             <Governance />
             <Reporting />
+            <Footer />
         </>
     )
 }
