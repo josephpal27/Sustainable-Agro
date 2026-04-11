9ac92678e5d1b9a5d78deb811497678f96966bc2
Joseph Pal <josephpal27@gmail.com>
2026-04-11 12:26:35 +0530

    Banners Updated

--- src/components/careers/careers-banner/CareersBanner.jsx
+++ src/components/careers/careers-banner/CareersBanner.jsx
@@ -4,7 +4,7 @@ const CareersBanner = () => {
     return (
         <>
             <section className="careers-banner">
-                <img src="/images/banners/careers-banner.png" alt="Careers Banner" loading="eager" />
+                <img src="/images/banners/career-banner.avif" alt="Careers Banner" loading="eager" />
                 <div className="banner-content">
                     <h1 data-aos="fade-up">CAREER</h1>
                     <p data-aos="fade">Join a team that is redefining the future of our planet. At Sustainable Agro, you will work at the cutting edge of climate-tech and agronomy. We offer a collaborative, fast-paced environment where your work directly contributes to global sustainability goals.</p>
