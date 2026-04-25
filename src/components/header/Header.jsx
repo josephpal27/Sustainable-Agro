9e3349905dc3a57690172a6d13802a85879752df
Joseph Pal <josephpal27@gmail.com>
2026-04-25 15:42:02 +0530

    Header Updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import './Header.css';
 
-import { IoHomeSharp } from "react-icons/io5";
+import { IoHomeSharp, IoMailOpen } from "react-icons/io5";
 import { FaPhone, FaFacebookF, FaInstagram, FaXTwitter } from "react-icons/fa6";
 
 const Header = () => {
@@ -12,12 +12,12 @@ const Header = () => {
         </div>
         <div className="header-right">
             <div className="head-box">
-                <IoHomeSharp className="icon" />
-                <p>202 Park Avenue, New York City</p>
+                <IoMailOpen className="icon"/>
+                <p>info@sustainableagro.net</p>
             </div>
             <div className="head-box">
                 <FaPhone className="icon" />
-                <p>+91 123 456 7890</p>
+                <p>+225 05073 81923</p>
             </div>
             <div className="social-icons">
                 <a href="#" target="_blank"><FaFacebookF className="icon" /></a>
